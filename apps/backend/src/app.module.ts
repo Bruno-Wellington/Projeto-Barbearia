1dfe1619ad194d0610594a20636d7d0b11a3b722
Bruno Carvalho <brunocarvalho.desenv@gmail.com>
2024-07-27 11:14:40 -0300

    Implementações no Backend da aplicação

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DbModule } from './db/db.module';
 import { ServicoModule } from './servico/servico.module';
+import { AgendamentoModule } from './agendamento/agendamento.module';
 
 @Module({
-  imports: [DbModule, ServicoModule],
+  imports: [DbModule, ServicoModule, AgendamentoModule],
   controllers: [],
   providers: [],
 })
